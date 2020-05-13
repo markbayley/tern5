9a559347d4b387a3831d0c4e0bc37c684253bed5
markbayley <markyb152@hotmail.com>
2020-05-13 21:03:39 +1000

    Commit Convert

--- src/App.js
+++ src/App.js
@@ -1,39 +1,71 @@
-import React, { Component, Fragment } from 'react';
-import NavBar from './components/NavBar.jsx';
-import { Container } from 'semantic-ui-react';
-import logo from './logo@3x.png';
+import React from 'react';
 import './App.css';
 
-
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <Fragment>
-        <NavBar />
-        <Container className='main'>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
+      <div className="App">
 
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+        <div class="above-header">
+          <div class="container">
+            <div class="above-header-section-wrap d-flex">
+              <div class="above-header-section above-header-section-1">
+                <div class="user-select">
+                  <div class="ast-custom-html"><a href="#"><img src="img/logo-mini-all.png" alt="" /></a></div>
+                </div>
+              </div>
 
-              </a>
-            </header>
+              <div class="above-header-section above-header-section-2">
+                <div class="user-select">
+                  <div class="ast-custom-html"><a href="#" target="_blank" class="btn-orange">Data</a></div>
+                </div>
+              </div>
+            </div>
           </div>
-        </Container>
-      </Fragment>
+        </div>
+        <div class="main-header">
+          <div class="container">
+            <div class="main-header-container d-flex">
+              <div class="site-branding">
+                <a href="#"><span><img src="img/logo@3x.png" alt="" /></span></a>
+              </div>
+              <div class="main-header-bar-navigation">
+                <input class="menu-btn" type="checkbox" id="menu-btn" />
+                <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+                <ul id="primary-menu" class="main-header-menu d-flex">
+                  <li><a href="#">About</a>
+                    <ul class="dropdown">
+                
+                    </ul>
+                  </li>
+                  <li><a href="#">TERN Observatory</a>
+                    <ul class="dropdown">
+                  
+                    </ul>
+                  </li>
+                  <li><a href="#">Tools & Resources</a>
+                    <ul class="dropdown">
+                 
+                    </ul>
+                  </li>
+                  <li><a href="#">Community</a>
+                    <ul class="dropdown">
+                 
+                    </ul>
+                  </li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
 
-    );
 
-  }
+ 
 
-}
-
-export default App;
+                </div>
+                );
+              }
+              }
+              
+              export default App;
