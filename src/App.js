41eaa3b66320ac0a644fe02073e799e254a55728
markbayley <markyb152@hotmail.com>
2020-05-13 21:50:57 +1000

    Commit 2

--- src/App.js
+++ src/App.js
@@ -23,6 +23,9 @@ class App extends React.Component {
             </div>
           </div>
         </div>
+
+
+
         <div class="main-header">
           <div class="container">
             <div class="main-header-container d-flex">
@@ -35,22 +38,22 @@ class App extends React.Component {
                 <ul id="primary-menu" class="main-header-menu d-flex">
                   <li><a href="#">About</a>
                     <ul class="dropdown">
-                
+
                     </ul>
                   </li>
                   <li><a href="#">TERN Observatory</a>
                     <ul class="dropdown">
-                  
+
                     </ul>
                   </li>
                   <li><a href="#">Tools & Resources</a>
                     <ul class="dropdown">
-                 
+
                     </ul>
                   </li>
                   <li><a href="#">Community</a>
                     <ul class="dropdown">
-                 
+
                     </ul>
                   </li>
                   <li><a href="#">Contact</a></li>
@@ -61,11 +64,85 @@ class App extends React.Component {
         </div>
 
 
- 
 
+        <div class="main-footer">
+          <div class="container">
+            <div class="main-footer-row">
+              <div class="main-footer-container">
+                <div class="footer-col footer-column-1">
+                  <div class="footer-col-inner">
+                    <div class="footer-col-inner-border">
+                      <h3>Contact us</h3>
+                      <p class="mb-20">
+                        Physical &amp; Mail Address
+                        <div>Room 156, Goddard Building (8)The University of Queensland</div>
+                        <p><span >St Lucia 4072</span></p>
+                        <p class="fz-15 font-500 mb-20">P <a class="text-black" href="tel:0733659097">(07) 33659097</a>E <a href="mailto:esupport@tern.org.au">esupport@tern.org.au</a></p>
+                        <p class="fz-15"><a href="https://www.tern.org.au/access-policy">Access Policy</a></p>
+                        <ul class="social-icons">
+                          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                          <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                );
-              }
-              }
-              
-              export default App;
+                <div class="footer-col footer-column-2">
+                  <div class="footer-col-inner">
+                    <div class="footer-col-inner-border">
+                      <h3>Funding</h3>
+                      <p class="mb-46">TERN is supported by the Australian Government through the National
+									Collaborative Research Infrastructure Strategy, <span
+                          style={{ textDecoration: "underline" }}><a
+                            href="https://www.education.gov.au/national-collaborative-research-infrastructure-strategy-ncris">NCRIS</a></span>.
+								</p>
+                      <a
+                        href="https://www.education.gov.au/national-collaborative-research-infrastructure-strategy-ncris">
+                        <img width="138" src="img/logo-ncris@2x.png" alt="" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="footer-col footer-column-3">
+                  <div class="footer-col-inner">
+                    <div class="footer-col-inner-border">
+                      <h3>Co-investment</h3>
+                      <img width="180" src="img/Coinvestment-lockup.jpg" alt="" />
+                    </div>
+                  </div>
+                </div>
+                <div class="footer-col footer-column-4">
+                  <div class="footer-col-inner">
+                    <div class="footer-col-inner-border">
+                      <h3>Newsletter</h3>
+                      <p>Subscribe for project updates, data releases, research findings, and users stories
+									direct to your inbox.</p>
+                      <div class="subscription-form">
+                        <form action="">
+                          <input type="text" name="email" value="" placeholder="Email Address" />
+
+                          <input type="submit" value="Subscribe to TERN" />
+                        </form>
+                      </div>
+
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+    );
+  }
+}
+
+export default App;
