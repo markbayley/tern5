9a01d933ec1599fc3f89227149fc45a6c151c7db
Gerhard Weis <g.weis@uq.edu.au>
2020-08-21 09:48:12 +1000

    cleanup and lint BioMapEngine fix osm attribution url

--- src/components/bio-image-map/BioMapEngine.js
+++ src/components/bio-image-map/BioMapEngine.js
@@ -1,38 +1,34 @@
 import React, { useState, useEffect } from "react";
-import ImageMarkerEngine from "./ImageMarkerEngine";
-import { Map, TileLayer, FeatureGroup, Circle } from "react-leaflet";
+import {
+  Map, TileLayer, FeatureGroup, Circle,
+} from "react-leaflet";
 import { EditControl } from "react-leaflet-draw";
 import Leaflet from "leaflet";
 import { useSelector, useDispatch } from "react-redux";
+import ImageMarkerEngine from "./ImageMarkerEngine";
 import { fetchSearchAction } from "../../store/reducer";
 
 const BioMapEngine = () => {
-  const [mapInitState, setMapInitState] = useState({
+  const [mapInitState] = useState({
     lat: -26.47,
     lng: 134.02,
     zoom: 5,
     maxZoom: 30,
     minZoom: 5,
   });
-  const [showMap, setShowMap] = useState(true);
-  const [showMapButtonLabel, setShowMapButtonLabel] = useState("Hide Map");
   const mapInitPosition = [mapInitState.lat, mapInitState.lng];
   const bioImageDocuments = useSelector((state) => state.search.hits);
   const selectedFilter = useSelector((state) => state.search.selectedFilter);
   const dispatch = useDispatch();
 
-  console.log("In BioMapEngine. bioImageDocumentHits=", bioImageDocuments);
-
-  //Set map boundary (australia)
+  // Set map boundary (australia)
   const corner1 = Leaflet.latLng(-9.820066, 115.240312);
   const corner2 = Leaflet.latLng(-44.482812, 152.339923);
   const bounds = Leaflet.latLngBounds(corner1, corner2);
 
   useEffect(() => {
-    console.log("in useEffect(). selectedFilter=", selectedFilter);
     dispatch(fetchSearchAction(selectedFilter));
-  }, []);
-
+  }, [dispatch, selectedFilter]);
 
   const BioMap = () => (
     <div className="map-frame">
@@ -52,27 +48,17 @@ const BioMapEngine = () => {
           minZoom={mapInitState.zoom}
           maxBounds={bounds}
         >
-          {/* <TileLayer
-            attribution='&copy; <a href="http://a.tile.openstreetmap.fr/hot/${z}/${x}/${y}.png">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          />
-
           <TileLayer
-            attribution='&copy; <a href="http://a.tile.openstreetmap.fr/hot/${z}/${x}/${y}.png">OpenStreetMap</a> contributors'
-            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-          /> */}
-
-          <TileLayer
-            attribution='&copy; <a href="http://a.tile.openstreetmap.fr/hot/${z}/${x}/${y}.png">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+            attribution='&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/hot/{z}/{x}/{y}.png"
           />
 
           <FeatureGroup>
             <EditControl
               position="bottomright"
-              // onEdited={this._onEditPath}
-              // onCreated={this._onCreate}
-              // onDeleted={this._onDeleted}
+            // onEdited={this._onEditPath}
+            // onCreated={this._onCreate}
+            // onDeleted={this._onDeleted}
             />
             <Circle center={[51.51, -0.06]} radius={200} />
           </FeatureGroup>
@@ -81,7 +67,7 @@ const BioMapEngine = () => {
           {bioImageDocuments.map((bioImageDocument) => (
             <ImageMarkerEngine
               bioImageDocument={bioImageDocument["_source"]}
-              siteLocation={bioImageDocument["_source"]["site_id"]["value"]}
+              siteLocation={bioImageDocument["_source"]["site_id"].value}
               key={bioImageDocument["_id"]}
             />
           ))}
