25ba8d5439cb2852d06dd121ddebaf527eb66da0
Mosheh EliYahu <mosheh.eliyahu@adelaide.edu.au>
2020-08-25 12:44:16 +0930

    Feat: Added date range searching. More testing required though

--- src/components/bio-image-search/DateRange.jsx
+++ src/components/bio-image-search/DateRange.jsx
@@ -1,74 +1,91 @@
-import React from "react";
-// import PropTypes from "prop-types";
-// import "./App.css";
-
-// eslint-disable-next-line import/no-unassigned-import
+/* eslint import/no-unassigned-import: "off" */
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "react-dates/initialize";
 import { DateRangePicker } from "react-dates";
 import "react-dates/lib/css/_datepicker.css";
+// import moment from "moment";
+import { selectedFilterAction } from "../../store/reducer";
 
-class DateRange extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      startDate: null,
-      endDate: null,
-    };
-  }
+const DateRange = () => {
+  const dispatch = useDispatch();
+  const [dateRange, setDateRange] = useState({
+    start: null,
+    end: null,
+  });
+  const [focus, setFocus] = useState(null);
 
-  hundleDateChange(startDate, endDate) {
-    this.setState(() => ({
-      endDate,
-      startDate,
-    }));
-  }
+  const { start, end } = dateRange;
 
-  render() {
-    const { startDate, endDate, focusedInput } = this.state;
-    return (
-      <>
-        <div
+  const handleOnDateChange = ({ startDate, endDate }) => {
+    if (startDate) {
+      const sRange = { ...dateRange, ...{ start: startDate } };
+      setDateRange(sRange);
+    }
+    if (endDate) {
+      const sRange = { ...dateRange, ...{ end: endDate } };
+      setDateRange(sRange);
+    }
+  };
+
+  useEffect(() => {
+    // eslint says No!! const { start, end } = dateRange;
+    if (dateRange.start && dateRange.end) {
+      // Note: start and end are Moment types, so need to convert for API
+      const dateFrom = dateRange.start.format("YYYY-MM-DD");
+      const dateTo = dateRange.end.format("YYYY-MM-DD");
+      const dateRangeSearch = { date_from: dateFrom, date_to: dateTo };
+      dispatch(selectedFilterAction(dateRangeSearch));
+    } else {
+      // console.log("NOT READY! ", dateRange.start, dateRange.end);
+    }
+  }, [dateRange, dispatch]);
+
+  return (
+    <>
+      <div
+        style={{
+          // borderRight: "55px solid rgba(149, 219, 199, 0.5)",
+          marginLeft: "23px",
+        }}
+      >
+        <h6
           style={{
-            // borderRight: "55px solid rgba(149, 219, 199, 0.5)",
-            marginLeft: "23px",
+            paddingTop: "15px",
+            color: "#065f65",
+            fontWeight: "500",
           }}
         >
-          <h6
-            style={{
-              paddingTop: "15px",
-              color: "#065f65",
-              fontWeight: "500",
-            }}
-          >
-            Date Range
-          </h6>
-
-          <DateRangePicker
-            small
-            noBorder
-            keepOpenOnDateSelect
-            startDateAriaLabel="fff"
-            startDate={startDate}
-            startDateId="start_date_id"
-            endDate={endDate}
-            endDateId="end_date_id"
-            onDatesChange={
-              ({ newStartDate, newEndDate }) => this.hundleDateChange(newStartDate, newEndDate)
-            }
-            focusedInput={focusedInput}
-            onFocusChange={(newFocusedInput) => this.setState({ focusedInput: newFocusedInput })}
-          />
-        </div>
-        <hr
-          style={{
-            border: "0.5px solid #66b3a6",
-            marginTop: "0%",
-            marginBottom: "0%",
-            marginLeft: "5px",
-          }}
+          Date Range
+        </h6>
+        <DateRangePicker
+          small
+          noBorder
+          keepOpenOnDateSelect
+          startDateAriaLabel="fff"
+          startDate={start}
+          startDateId="start_date_id"
+          endDate={end}
+          endDateId="end_date_id"
+          onDatesChange={handleOnDateChange}
+          focusedInput={focus}
+          onFocusChange={(focusV) => setFocus(focusV)}
+          startDatePlaceholderText="Start"
+          endDatePlaceholderText="End"
+          numberOfMonths={2}
+          isOutsideRange={() => false}
         />
-      </>
-    );
-  }
-}
+      </div>
+      <hr
+        style={{
+          border: "0.5px solid #66b3a6",
+          marginTop: "0%",
+          marginBottom: "0%",
+          marginLeft: "5px",
+        }}
+      />
+    </>
+  );
+};
+
 export default DateRange;
